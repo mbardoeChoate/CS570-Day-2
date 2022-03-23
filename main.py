f035840bbcda917de480f18fab52c87231bacc57
mbardoe <mbardoe@gmail.com>
2022-03-23 05:47:25 -0400

    initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+# Day-2 Code
+
+def greeting(my_name):
+    print(f"Hi, {my_name}")
+
+def greeting_list(my_names):
+    pass
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    greeting('PyCharm')
